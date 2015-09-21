b6bc3e03bea0c68efb60a7faf34f2bb24342d591
Vador05 <sal.beltran.05@gmail.com>
2015-09-21 13:47:47 +0200

    Hola

---
+++ src/main/java/edu/upc/eetac/dsa/sbeltran/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.sbeltran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
